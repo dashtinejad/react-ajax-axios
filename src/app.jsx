5846b7972d6ab4082e447cf1388a2d6200594fe9
Mojtaba Dashtinejad <mojtaba@gmail.com>
2017-01-30 15:35:55 +0330

    componentDidMount and componentDidUpdate lifecycle

--- src/app.jsx
+++ src/app.jsx
@@ -8,16 +8,26 @@ class Repos extends React.Component {
     this.state = {repos: []};
   }
 
-  componentDidMount() {
-    axios.get('https://api.github.com/users/facebook/repos')
+	fetchRepos() {
+		axios.get(`https://api.github.com/users/${this.props.username}/repos`)
       .then(response => this.setState({ repos: response.data }))
       .catch(error => console.log(error));
+	}
+
+  componentDidMount() {
+    this.fetchRepos();
   }
+
+	componentDidUpdate(prevProps) {
+		if (this.props.username != prevProps.username) {
+			this.fetchRepos();
+		}
+	}
 
   render() {
     return (
       <div>
-        <h1>Repos by facebook</h1>
+        <h1>Repos by {this.props.username}</h1>
         {this.state.repos.map(repo =>
           <div key={repo.id}>{repo.name}</div>
         )}
@@ -26,4 +36,26 @@ class Repos extends React.Component {
   }
 }
 
-ReactDOM.render(<Repos />, document.getElementById('app'))
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+    this.state = {username: 'facebook'};
+	}
+
+	render() {
+    return (
+      <div>
+				<button onClick={() => this.setState({username: 'facebook'})}>Facebook</button>
+				<button onClick={() => this.setState({username: 'microsoft'})}>Microsoft</button>
+				<button onClick={() => this.setState({username: 'google'})}>Google</button>
+
+        <Repos username={this.state.username} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+)
